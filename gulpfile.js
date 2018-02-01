075e4b123da5bd2674f1aab76931538743614ebf
dalcaraz <damian.alcaraz@dmsti.es>
2018-02-01 10:04:31 +0100

    upload files to repo

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require ('gulp');
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+  gulp.src('./sass/base.sass')
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }))
+    .pipe(autoprefixer({
+      versions: ['last 2 browsers']
+    }))
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task('default', function(){
+  gulp.watch('./sass/*.sass', ['sass']);
+});
