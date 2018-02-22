40a73a2195cf45ec3fab1da0227e58f88e2c1b6f
dalcaraz <damian.alcaraz@dmsti.es>
2018-02-22 13:05:36 +0100

    update js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  
+  $("#txtInput").val();
+  var txtInputVal = $("#txtInput").val();
+  var arrBin = [];
+
+  $("#btnInput").click( function(){
+    var txtInputValNew = txtInputVal / 2;
+    var getRest = ~~(txtInputVal / 2);
+    alert(txtInputVal);
+    while(txtInputVal > 2){
+      if(txtInputValNew > 2){
+        txtInputValNew / 2;
+        getRest;
+        arrBin.push(getRest);
+      } else if (txtInputValNew == 2){
+        getRest;
+        arrBin.push(getRest, txtInputValNew);
+      };
+    };
+    $("#result").html(arrBin.reverse());
+  });
+
 });
