b8b0038d4f0f9b8f6c4bbe4d76d0c348369e6530
dalcaraz <damian.alcaraz@dmsti.es>
2018-03-15 13:51:02 +0100

    update files

--- js/js.js
+++ js/js.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 
+  var synth_fm = new Tone.FMSynth().toMaster()
+
+  $("div").each(function(){
+    $(this).mousedown(function(e){
+      synth_fm.triggerAttack(e.target.textContent);
+    })
+    $(this).mouseup(function(e){
+      synth_fm.triggerRelease();
+    })
+  })
+
 });
