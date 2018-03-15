9aa88d9b28a5d3384d0e1528cd5a3b9961c623a8
dalcaraz <damian.alcaraz@dmsti.es>
2018-03-15 12:42:55 +0100

    upload files

--- js/js.js
+++ js/js.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
-  $("#btnCheck").click( function(){
+  let a;
+  let b;
+  let c;
 
-    var txtInputVal = $("#txtInput").val();
-    var txtInputValNew = txtInputVal;
-    var arrBin = [];
+  // function timer_1 (100, a, {alumno_1 : 1, alumno_2 : 2}){};
+  // function timer_2 (200, b, [2,4,8]){};
+  // function timer_3 (400, c, [12,16]){};
 
-    while(txtInputValNew >= 2){
-      var getRest = txtInputValNew % 2;
-      Math.floor(txtInputVal / 2);
-      arrBin.push(getRest);
-      txtInputValNew = Math.floor(txtInputValNew / 2);
-    };
+  function timer_1(){
+    console.log("Hello 1");
+  };
+  function timer_2(){
+    console.log("Hello 2");
+  };
+  function timer_3(){
+    console.log("Hello 3");
+  };
 
-    arrBin.push(1);
-    result = arrBin.reverse();
+  function startTimer(){
+      a = setTimeout(timer_1, 1000);
+      b = setTimeout(timer_2, 2000);
+      c = setTimeout(timer_3, 4000);
+  }
 
-    $("#result").html(result);
-  });
+  $("button").on("click", startTimer);
 });
