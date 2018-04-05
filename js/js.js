c790e9243e9b5386ac975105b0f4e21abde9ce1f
Damian Alcaraz Garcia <damian.alcaraz@nateevo.com>
2018-04-05 18:22:44 +0200

    first & last commit

--- js/js.js
+++ js/js.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
+  // $("#sprite-change-1").hover(function(){
+  //   $("#sprite").css({"background-position":"0 0"});
+  // });
+  // $("#sprite-change-2").hover(function(){
+  //   $("#sprite").css({"background-position":"-256px 0"});
+  // });
+  // $("#sprite-change-3").hover(function(){
+  //   $("#sprite").css({"background-position":"-512px 0"});
+  // });
+  // $("#sprite-change-4").hover(function(){
+  //   $("#sprite").css({"background-position":"-768px 0"});
+  // });
+
+  // $("#sprite").mousemove(function(e){
+  //   var pageCoords = "( " + e.pageX + ", " + e.pageY + " )";
+  //   console.log(pageCoords);
+  // });
+
+  $("#sprite").mousemove(function(e){
+    if(e.pageY < 42){
+      $("#sprite").css({"background-position":"0 0"});
+    }else if(e.pageY > 43 && e.pageY < 84){
+      $("#sprite").css({"background-position":"-256px 0"});
+    }else if(e.pageY > 85 && e.pageY < 126){
+      $("#sprite").css({"background-position":"-512px 0"});
+    }else if(e.pageY > 127 && e.pageY < 168){
+      $("#sprite").css({"background-position":"-768px 0"});
+    }
+  });
 
 });
