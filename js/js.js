0a3b955776fe921cc2b5336599d4408954091011
Damian Alcaraz Garcia <damian.alcaraz@nateevo.com>
2018-04-09 14:00:10 +0200

    'add_dropdown_functionality_with_arrows'

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-
+$(".arrow-down").on("click", function(){
+  $(".arrow-down").css({"display":"none"});
+  $(".arrow-up").css({"display":"block"});
+  $(".table-data-sheet").css({"display":"block"});
+});
+$(".arrow-up").on("click", function(){
+  $(".arrow-down").css({"display":"block"});
+  $(".arrow-up").css({"display":"none"});
+  $(".table-data-sheet").css({"display":"none"});
 });
